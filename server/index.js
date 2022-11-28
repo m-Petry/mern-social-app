36ff5e4a82a9269fb7f29c6bfaadffba331e4e01
Marcelo Petry <marcelospetry@gmail.com>
2022-11-27 21:16:04 -0300

    auth of routes configured

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./toures/auth.js";
 import { register } from "./controllers/auth.js";
 
 /* Configurations */
@@ -38,6 +39,9 @@ const upload = multer({ storage });
 
 // Routes with files
 app.post("/auth/register", upload.single("picture"), register); //middlaware example
+
+//Routes
+add.post("/auth", authRoutes);
 
 // Mongoose Setup
 const PORT = process.env.PORT || 6001; //port backup
