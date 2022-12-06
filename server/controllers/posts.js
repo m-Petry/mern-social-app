a9c37814c8a24b20c284808c2f633101570c122f
Marcelo Petry <marcelospetry@gmail.com>
2022-12-05 23:22:23 -0300

    Back End Completed!

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,77 @@
 import Post from "../models/Post.js";
+import User from "../models/User.js";
+
+// Create Post
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.picturePath,
+      picturePath,
+      likes: {},
+      coments: []
+    });
+    await newPost.save(); //save the post into mongodb
+
+    const post = await Post.find(); //grab all the the posts
+    res.status(201).json(post); //return the post
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
+
+// Read
+export const getFeedPosts = async (req, res) => {
+  //get news feed for user
+  try {
+    const post = await Post.find();
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+// Only grab the user id posts
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const post = await Post.find({ userId });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+// Update
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true);
+    }
+
+    //update a specific post
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
