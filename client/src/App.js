7ebfc1ee6d620eeaf3cb68e6a4c37c0a844efcd9
Marcelo Petry <marcelospetry@gmail.com>
2022-12-11 10:39:04 -0300

    bug json fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter></BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
